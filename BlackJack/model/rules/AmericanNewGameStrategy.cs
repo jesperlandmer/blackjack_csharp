34635d3041c29e4e666516d4ce9814ad0dc1e86d
william <c.william.robertson@gmail.com>
2017-10-23 18:13:45 +0200

    Added private methods to give cards to dealer and player.

--- BlackJack/model/rules/AmericanNewGameStrategy.cs
+++ BlackJack/model/rules/AmericanNewGameStrategy.cs
@@ -9,25 +9,28 @@ namespace BlackJack.model.rules
     {
         public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
-            Card c;
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_dealer.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(false);
-            a_dealer.DealCard(c);
+            
+            givePlayerCard(a_deck, a_player, true);
+            giveDealerCard(a_deck, a_dealer, true);
+            givePlayerCard(a_deck, a_player, true);
+            giveDealerCard(a_deck, a_dealer, false);
 
             return true;
+        }
+        private void givePlayerCard(Deck a_deck, Player a_player, bool show)
+        {
+            Card c;
+            c = a_deck.GetCard();
+            c.Show(show);
+            a_player.DealCard(c);
+        }
+
+        private void giveDealerCard(Deck a_deck, Dealer a_dealer, bool show)
+        {
+            Card c;
+            c = a_deck.GetCard();
+            c.Show(true);
+            a_dealer.DealCard(c);
         }
     }
 }
