afbb96f8324404edd9188d27db377f6bf2d36525
william <c.william.robertson@gmail.com>
2017-10-23 18:56:54 +0200

    implementation not working

--- BlackJack/model/rules/AmericanNewGameStrategy.cs
+++ BlackJack/model/rules/AmericanNewGameStrategy.cs
@@ -9,31 +9,28 @@ namespace BlackJack.model.rules
     {
         public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
-            // a_player.DealCard(givecard(a_deck.GetCard(), true));
-
-            // a_dealer.DealCard(givecard(a_deck.GetCard(), true))
-            givePlayerCard(a_deck.GetCard(), a_player, true);
-            giveDealerCard(a_deck.GetCard(), a_dealer, true);
-            givePlayerCard(a_deck.GetCard(), a_player, true);
-            giveDealerCard(a_deck.GetCard(), a_dealer, false);
+            
+            givePlayerCard(a_deck, a_player, true);
+            giveDealerCard(a_deck, a_dealer, true);
+            givePlayerCard(a_deck, a_player, true);
+            giveDealerCard(a_deck, a_dealer, false);
 
             return true;
         }
-        public Card givecard(Card card, bool doShow)
+        public void givePlayerCard(Deck a_deck, Player a_player, bool doShow)
         {
-            card.Show(doShow);
-            return card;
-        }
-        public void givePlayerCard(Card card, Player a_player, bool doShow)
-        {
-            card.Show(doShow);
-            a_player.DealCard(card);
+            Card c;
+            c = a_deck.GetCard();
+            c.Show(doShow);
+            a_player.DealCard(c);
         }
 
-        public void giveDealerCard(Card card, Dealer a_dealer, bool doShow)
+        public void giveDealerCard(Deck a_deck, Dealer a_dealer, bool doShow)
         {
-            card.Show(doShow);
-            a_dealer.DealCard(card);
+            Card c;
+            c = a_deck.GetCard();
+            c.Show(doShow);
+            a_dealer.DealCard(c);
         }
     }
 }
--- BlackJack/model/rules/InternationalNewGameStrategy.cs
+++ BlackJack/model/rules/InternationalNewGameStrategy.cs
@@ -5,28 +5,32 @@ using System.Text;
 
 namespace BlackJack.model.rules
 {
-  class InternationalNewGameStrategy : INewGameStrategy
-  {
-
-    public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
+    class InternationalNewGameStrategy : INewGameStrategy
     {
-      givePlayerCard(a_deck.GetCard(), a_player, true);
-      giveDealerCard(a_deck.GetCard(), a_dealer, true);
-      givePlayerCard(a_deck.GetCard(), a_player, true);
 
-      return true;
-    }
+        public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
+        {
+            givePlayerCard(a_deck, a_player, true);
+            giveDealerCard(a_deck, a_dealer, true);
+            givePlayerCard(a_deck, a_player, true);
 
-    public void givePlayerCard(Card card, Player a_player, bool doShow)
-    {
-      card.Show(doShow);
-      a_player.DealCard(card);
-    }
+            return true;
+        }
 
-    public void giveDealerCard(Card card, Dealer a_dealer, bool doShow)
-    {
-      card.Show(doShow);
-      a_dealer.DealCard(card);
+        public void givePlayerCard(Deck a_deck, Player a_player, bool doShow)
+        {
+            Card c;
+            c = a_deck.GetCard();
+            c.Show(doShow);
+            a_player.DealCard(c);
+        }
+
+        public void giveDealerCard(Deck a_deck, Dealer a_dealer, bool doShow)
+        {
+            Card c;
+            c = a_deck.GetCard();
+            c.Show(doShow);
+            a_dealer.DealCard(c);
+        }
     }
-  }
 }
