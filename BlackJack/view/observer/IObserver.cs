4127e1e273be85b238055a70ec18d5c3d2cd9d77
william <c.william.robertson@gmail.com>
2017-10-24 20:20:16 +0200

    changes made

--- BlackJack/view/observer/IObserver.cs
+++ BlackJack/view/observer/IObserver.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace BlackJack.view
 {
-    interface IObserver
+  interface IObserver
     {
         void PauseEventHandler();
     }
