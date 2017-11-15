f6cb3a8d3d8ae267aba5d7be73ac54bbb0d533ff
vincesthehuman <wr222ae@student.lnu.se>
2017-11-15 15:30:03 +0100

    added thread sleep and writelines so one can find bugz

--- BlackJack/model/observer/PlayerObserver.cs
+++ BlackJack/model/observer/PlayerObserver.cs
@@ -1,28 +1,39 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 
 namespace BlackJack.model.observer
 {
-    class PlayerObserver
-    {
-        private List<IObserver> _observers = new List<IObserver>();
+  class PlayerObserver
+  {
+    public List<IObserver> _observers = new List<IObserver>();
 
-        public void AddObserver(IObserver observer)
-        {
-            _observers.Add(observer);
-        }
-        public void DeleteObserver(IObserver observer)
-        {
-            _observers.Remove(observer);
-        }
-        public void NotifyObserver(List<Card> m_hand)
-        {
-            Console.WriteLine("poo");
-            foreach(IObserver o in _observers)
-            {
-                o.Update(m_hand);
-            }
-        }
+    public void AddObserver(IObserver observer)
+    {
+      Console.WriteLine("Observer added");
+      _observers.Add(observer);
+      Console.WriteLine("How many observers: ");
+      Console.WriteLine(_observers.Count);
+      Thread.Sleep(5000);
     }
+    public void DeleteObserver(IObserver observer)
+    {
+      _observers.Remove(observer);
+    }
+    public void NotifyObserver(List<Card> m_hand)
+    {
+      Thread.Sleep(2000);
+      Console.WriteLine("Notified");
+      Console.WriteLine("How many observers: ");
+      Console.WriteLine(_observers.Count);
+      foreach (IObserver o in _observers)
+      {
+        o.Update(m_hand);
+        Console.WriteLine("This part never runs since _observers is empty");
+        Thread.Sleep(2000);
+
+      }
+    }
+  }
 }
